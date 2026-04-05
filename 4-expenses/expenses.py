8e70cd5109a70b84fe9074f865cc550411017a40
Vadim Ivlev <vimloko@Mac-mini-Vadim.local>
2026-04-05 22:45:04 +0500

    Домашнее задание - Строки (исправление)

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -11,17 +11,23 @@
 - На выходе — сумма в виде X.YY ₽ (два знака после запятой)
 - Если формат некорректный — вывести: Некорректный формат суммы
 """
+
+
 user_summa: str = input("Введите сумму: ").strip().lower()
 
 splitted_user_summa: list[str] = user_summa.split()
 monetary_units: list[str] = splitted_user_summa[1::2]
 rub: str
 kop: str
+result: str = "Некорректный формат суммы"
 if "руб" in monetary_units and "коп" in monetary_units:
     rub, kop = splitted_user_summa[::2]
-    print(f"{rub}.{kop} ₽")
-elif "руб" in monetary_units and "коп" not in monetary_units:
+    if rub.isdigit() and kop.isdigit():
+        result = f"{rub}.{kop.zfill(2)} ₽"
+
+if "руб" in monetary_units and "коп" not in monetary_units:
     rub = splitted_user_summa[0]
-    print(f"{float(rub):.2f} ₽")
-else:
-    print("Некорректный формат суммы")
+    if rub.isdigit():
+        result = f"{float(rub):.2f} ₽"
+
+print(result)
